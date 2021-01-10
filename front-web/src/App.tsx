0ce0f0e5d3259c1faa4faf93b677442965199028
matheus <hmatheus00@outlook.com>
2021-01-10 11:15:01 -0300

    add home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './Navbar';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
     </div>
   );
 }
